e8c14f2ff3a770dbccfae4df278ea136f9b4092b
grzechuhehe <hubertgrzego@gmail.com>
2025-06-08 14:59:13 +0200

    removed unnecesary files

--- src/main/java/com/example/MainApplication.java
+++ src/main/java/com/example/MainApplication.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Map;
 
-public class git MainApplication {
+public class MainApplication {
 
     public static void main(String[] args) {
         FileLoader fileLoader = new FileLoader();
